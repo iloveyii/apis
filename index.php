49becbd57a2155aa5a7566ab6ef4bf94a6afca80
Hazrat Ali <iloveyii@yahoo.com>
2019-01-14 21:03:57 +0100

    Added screenshot png file

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<form class="form-signin">
+    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Please click a link</h1>
+    <a class="btn btn-lg btn-primary btn-block" href="/rest/">REST</a>
+    <a class="btn btn-lg btn-success btn-block" href="/rpc/">RPC</a>
+    <a class="btn btn-lg btn-info btn-block" href="/soap/">SOAP</a>
+    <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
+</form>
+</body>
+</html>
